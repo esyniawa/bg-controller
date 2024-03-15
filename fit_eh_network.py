9b4b3581bfb5e39c9ff0a600b4764ca8d0b1af90
syer <erik.syniawa@informatik.tu-chemnitz.de>
2024-03-15 10:45:54 +0100

    Fixed error function. Conda import Problems with ANNarchy

--- fit_eh_network.py
+++ fit_eh_network.py
@@ -39,7 +39,7 @@ def fit_reservoir(folder: str,
                   init_eta=0.0005,
                   exploratory_noise=0.5,
                   chaos_res=1.0,
-                  learning_time=10. * 1000.,  # 50 s
+                  learning_time=5. * 1000.,  # 5 s
                   test_time=2000.,
                   do_test=True) -> None:
 
@@ -89,7 +89,7 @@ def fit_reservoir(folder: str,
         ann.simulate(test_time)
 
         tracking = performance.get()
-        error = np.mean(tracking['p_output_pop']) - np.std(tracking['p_output_pop'])
+        error = np.log(np.mean(-tracking['p_output_pop']) + np.std(tracking['p_output_pop']) + np.amax(-tracking['p_output_pop']))
 
         performance.stop()
         ann.reset()
@@ -132,8 +132,8 @@ def fit_reservoir(folder: str,
     target = loss_function
 
     bads = BADS(target, np.array(my_params),
-                lower_bounds=np.array((0.000001, 0, 0)),
-                upper_bounds=np.array((1., 2., 5.)))
+                lower_bounds=np.array((0.0000001, 0., 0.)),
+                upper_bounds=np.array((1., 2.5, 5.)))
 
     optimize_result = bads.optimize()
     fitted_params = optimize_result['x']
