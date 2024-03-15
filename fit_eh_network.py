Works now. Init w_rec was wrong. ANNarchy Normal function takes the sd not the variance.

fit_eh_network.py
@@ -39,8 +39,8 @@ def fit_reservoir(folder: str,
                   init_eta=0.0005,
                   exploratory_noise=0.5,
                   chaos_res=1.5,
-                  learning_time=10. * 1000.,  # 5 s
-                  test_time=2000.,
+                  learning_time=20. * 1000.,  # 5 s
+                  test_time=3. * 1000.,
                   do_test=True) -> None:
 
     my_params = (init_eta, exploratory_noise, chaos_res)
@@ -90,8 +90,7 @@ def fit_reservoir(folder: str,
         ann.simulate(test_time)
 
         tracking = performance.get()
-        error = (np.log(np.mean(-tracking['p_output_pop'])) + np.log(np.std(tracking['p_output_pop'])) +
-                 np.log(np.amax(-tracking['p_output_pop'])))
+        error = np.log(np.mean(-tracking['p_output_pop'])) + np.log(np.amax(-tracking['p_output_pop']))
 
         performance.stop()
         ann.reset(monitors=True)
@@ -152,5 +151,5 @@ def fit_reservoir(folder: str,
 
 if __name__ == '__main__':
 
-    results_folder = f"results/run_{simID}/"
+    results_folder = f"results/fit_{simID}/"
     fit_reservoir(folder=results_folder)

network/model.py
@@ -18,7 +18,7 @@ target_pop = ann.Population(geometry=N_neurons_output, neuron=test_target_neuron
 reservoir = ann.Population(geometry=N_neurons_reservoir, neuron=reservoir_model, name='reservoir_pop')
 recurrent_res = ann.Projection(pre=reservoir, post=reservoir, target='rec')
 recurrent_res.connect_fixed_probability(probability=connect_prop,
-                                        weights=ann.Normal(0, 1/(connect_prop * N_neurons_reservoir)),
+                                        weights=ann.Normal(0, np.sqrt(1/(connect_prop * N_neurons_reservoir))),
                                         allow_self_connections=True)
 
 # output population

test_eh_network.py
@@ -6,14 +6,14 @@ simID = int(sys.argv[1])
 np.random.seed(simID)
 
 import ANNarchy as ann
-ann.setup(method='midpoint', num_threads=2)
+ann.setup(method='rk4', num_threads=1)
 
 # import from scripts
 from network.model import *
 from monitoring import PopMonitor, ConMonitor
 
-learning_time = 10. * 1000.  # 50 s
-test_time = 2000.
+learning_time = 20. * 1000.  # 50 s
+test_time = 20. * 1000.
 
 # save results in...
 results_folder = f'results/run_{simID}/'
@@ -38,14 +38,12 @@ weights = PopMonitor([res_output_proj], variables=['w'], sampling_rate=20.0)
 rates.start()
 weights.start()
 
-# con_monitor = ConMonitor([res_output_proj])
-# con_monitor.extract_weights()
-
 # init
 ann.disable_learning()
-ann.simulate(50.)
+ann.simulate(20.)
 
 # learning
+ann.set_time(0, net_id=0)  # to align target function with training and testing phase
 ann.enable_learning()
 ann.simulate(learning_time)
 
@@ -61,6 +59,3 @@ rates.save(results_folder)
 
 weights.stop()
 weights.save(results_folder)
-
-# con_monitor.extract_weights()
-# con_monitor.save_cons(results_folder)